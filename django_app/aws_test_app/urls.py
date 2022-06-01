a274b9e8c9a9daffb77caf04632cd68a9abd17c9
Hafiz Hassan Rehman (2207) <hassan.rehman@nxb.com.pk>
2022-06-01 18:17:27 +0500

    img tag added in index.html and image added in static folder

--- django_app/aws_test_app/urls.py
+++ django_app/aws_test_app/urls.py
@@ -1,14 +1,8 @@
-from django.urls import path, re_path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
-from django.conf import settings
+from django.urls import path
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
